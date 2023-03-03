f9ec68814db07637ae10c4bac4ab48a52e51b0fb
Digvijay Sethi <digvijaysethi1@gmail.com>
2023-03-03 13:33:33 +0530

    some changes in mui versions

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
 import React from "react";
 import { CssBaseline } from "@mui/material";
 import { Routes, Route } from "react-router-dom";
+import MovieInfo from "./components/MovieInfo/MovieInfo";
+import Actors from "./components/Actors/Actors";
+import Movies from "./components/Movies/Movies";
+import Profile from "./components/Profile/Profile";
+import NavBar from "./components/NavBar/NavBar";
+import useStyles from './styles';
 
 function App() {
+  const classes = useStyles();
   return (
-    <div>
+    <div className={classes.root}>
       <CssBaseline />
-      <h1>Hi this is Filmex</h1>
+      <NavBar />
+      <main className={classes.content}>
+        <div className={classes.toolbar} />
+        <Routes>
+          <Route path="/movie/:id" element={<MovieInfo />} />
+          <Route path="/actors/:id" element={<Actors />} />
+          <Route path="/" element={<Movies />} />
+          <Route path="/profile/:id" element={<Profile />} />
+        </Routes>
+      </main>
     </div>
   );
 }
